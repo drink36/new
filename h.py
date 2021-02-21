10dc7e9618e4557eb7df0b26d264c524f1eef672
drink36 <ooo910809@gmail.com>
2021-02-21 22:19:45 +0800

    Update h.py

--- h.py
+++ h.py
@@ -14,18 +14,5 @@ ue = UserAgent()
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 chrome = webdriver.Chrome(options=options)
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-TW,zh;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6",
-    "Host": "https://courseselection.ntust.edu.tw/",
-    "Referer": "https://www.learncodewithmike.com/",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-},
 chrome.get(url="https://courseselection.ntust.edu.tw/",
            headers={'ue': ue.random})
